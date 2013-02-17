d08ba15c1dc75a99b570d4e29ce8f8f42a6607ce
Casey Robinson <kc@rampantmonkey.com>
2013-02-17 15:05:31 -0500

    test lexer keywords

---
+++ test/lib/oryx/lexer_test.rb
@@ -0,0 +1,64 @@
+require_relative '../../test_helper'
+require 'rltk'
+
+module Oryx
+  class TestLexer < Test::Unit::TestCase
+    def compare(input, expected)
+      l = Lexer.new
+      l.lex(input).each_with_index do |t, i|
+        assert_equal expected[i].upcase, t.to_s
+      end
+    end
+
+    context "keywords" do
+      should "match boolean" do
+        compare("boolean", ["boolean", "eos"])
+      end
+      should "match char" do
+        compare("char", ["char", "eos"])
+      end
+      should "match else" do
+        compare("else", ["else", "eos"])
+      end
+      should "match false" do
+        compare("false", ["false", "eos"])
+      end
+      should "match if" do
+        compare("if", ["if", "eos"])
+      end
+      should "match int" do
+        compare("int", ["int", "eos"])
+      end
+      should "match print" do
+        compare("print", ["print", "eos"])
+      end
+      should "match return" do
+        compare("return", ["return", "eos"])
+      end
+      should "match string" do
+        compare("string", ["string", "eos"])
+      end
+      should "match true" do
+        compare("true", ["true", "eos"])
+      end
+      should "match void" do
+        compare("void", ["void", "eos"])
+      end
+      should "match while" do
+        compare("while", ["while", "eos"])
+      end
+    end
+
+    context "multiple keywords" do
+      should "match random selection of keywords in order" do
+        keywords = ["boolean", "char", "else", "false", "if", "int", "print", "return", "string", "true", "void", "while"]
+        100.times do
+          a = keywords.sample 5
+          compare a.join(" "), (a << "EOS")
+        end
+      end
+    end
+
+  end
+end
+
