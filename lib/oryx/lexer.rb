8a827e695cf4a26200f84a88b27fa40f4f7766d7
Casey Robinson <kc@rampantmonkey.com>
2013-02-17 15:04:08 -0500

    lexer keywords

--- lib/oryx/lexer.rb
+++ lib/oryx/lexer.rb
@@ -5,5 +5,19 @@ module Oryx
     # Skip whitespace
     rule(/\s/)
 
+    # Keywords
+    rule(/boolean/) { :BOOLEAN }
+    rule(/char/)    { :CHAR    }
+    rule(/else/)    { :ELSE    }
+    rule(/false/)   { :FALSE   }
+    rule(/if/)      { :IF      }
+    rule(/int/)     { :INT     }
+    rule(/print/)   { :PRINT   }
+    rule(/return/)  { :RETURN  }
+    rule(/string/)  { :STRING  }
+    rule(/true/)    { :TRUE    }
+    rule(/void/)    { :VOID    }
+    rule(/while/)   { :WHILE   }
+
   end
 end
