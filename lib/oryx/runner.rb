5231ae082f8f8b15f7639e2f58d875ab79a2d412
Casey Robinson <kc@rampantmonkey.com>
2013-02-18 15:15:12 -0500

    some helper functions to produce a prettier version of the token table

--- lib/oryx/runner.rb
+++ lib/oryx/runner.rb
@@ -21,5 +21,37 @@ module Oryx
         puts s
       end
     end
+    private
+      def tabularize_output tokens
+        s = table_header
+        tokens.each do |t|
+          line = ""
+          line += format " %-9s", t.type
+          line += " "
+          line += center t.value.to_s, 10
+          line += " "
+          line += center("#{t.position.line_number},#{t.position.line_offset}", 13) if t.position
+          s += line + "\n"
+        end
+        s
+      end
+
+      def table_header
+        s = center "TYPE", 10
+        s += "|"
+        s += center "VALUE", 10
+        s += "|"
+        s += center "POSITION", 12
+        s += "\n"
+        s += "-"*35
+        s += "\n"
+      end
+
+
+      def center text, width
+        left = (width - text.length)/2
+        right = width - text.length - left
+        " "*left + text + " "*right
+      end
   end
 end
