d65ea117bd0a40b5ae051238f2a35319fc7a4ef6
Casey Robinson <kc@rampantmonkey.com>
2013-03-21 10:20:41 -0400

    find numbers and variables

--- lib/oryx/ast.rb
+++ lib/oryx/ast.rb
@@ -3,5 +3,12 @@ require 'rltk/ast'
 module Oryx
   class Expression < RLTK::ASTNode; end
 
+  class Number < Expression
+    value :value, Integer
+  end
+
+  class Variable < Expression
+    value :name, String
+  end
 
 end
--- lib/oryx/parser.rb
+++ lib/oryx/parser.rb
@@ -4,5 +4,15 @@ require_relative "../oryx"
 module Oryx
   class Parser < RLTK::Parser
 
+    production(:statment) do
+      clause('e SEMI') { |e, _| e }
+    end
+
+    production(:e) do
+      clause('NUM')   { |i| Number.new i.to_i }
+      clause('IDENT') { |i| Variable.new i }
+    end
+
+    finalize
   end
 end
