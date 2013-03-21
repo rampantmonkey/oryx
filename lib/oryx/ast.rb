474977a199ac2d532fcf1084c85d6f8ed0b119ed
Casey Robinson <kc@rampantmonkey.com>
2013-03-21 10:21:27 -0400

    some binary operations

--- lib/oryx/ast.rb
+++ lib/oryx/ast.rb
@@ -11,4 +11,14 @@ module Oryx
     value :name, String
   end
 
+  class Binary < Expression
+    child :left, Expression
+    child :right, Expression
+  end
+
+
+  class Add < Binary; end
+  class Sub < Binary; end
+  class Mul < Binary; end
+  class Div < Binary; end
 end
--- lib/oryx/parser.rb
+++ lib/oryx/parser.rb
@@ -11,6 +11,11 @@ module Oryx
     production(:e) do
       clause('NUM')   { |i| Number.new i.to_i }
       clause('IDENT') { |i| Variable.new i }
+
+      clause('e PLUS e')  { |e0, _, e1|  Add.new(e0, e1) }
+      clause('e MINUS e') { |e0, _, e1|  Sub.new(e0, e1) }
+      clause('e TIMES e') { |e0, _, e1|  Mul.new(e0, e1) }
+      clause('e DIV e')   { |e0, _, e1|  Div.new(e0, e1) }
     end
 
     finalize
