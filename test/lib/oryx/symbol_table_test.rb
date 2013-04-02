e3fd04a56af1c4964a3fc69ccc0dcc48b4b77c94
Casey Robinson <kc@rampantmonkey.com>
2013-04-02 19:28:50 -0400

    Test exit_scope under 'normal' modes.
    For a correct program scope will always be larger than zero when
    exit_scope is called. These tests check that the scope level is
    correctly decremented.

--- test/lib/oryx/symbol_table_test.rb
+++ test/lib/oryx/symbol_table_test.rb
@@ -47,6 +47,27 @@ module Oryx
           st.exit_scope
         end
       end
+
+      should "decrease the scope" do
+        st.enter_scope
+        assert_nothing_raised do
+          st.exit_scope
+        end
+
+        assert_equal 0, st.current_scope
+      end
+
+      should "increase and decrease the scope many times" do
+        n = 73
+        (0..n).map do |i|
+          st.enter_scope
+          assert_equal i+1, st.current_scope
+          n-i
+        end.map do |i|
+          st.exit_scope
+          assert_equal i, st.current_scope
+        end
+      end
     end
 
   end
