55c849b79f5644792c5444a9e86374547a4cd637
Casey Robinson <kc@rampantmonkey.com>
2013-04-02 15:49:06 -0400

    test basics of symbol table

--- test/lib/oryx/symbol_table_test.rb
+++ test/lib/oryx/symbol_table_test.rb
@@ -2,5 +2,32 @@ require_relative '../../test_helper'
 
 module Oryx
   class TestSymbolTable < Test::Unit::TestCase
+    context "basics" do
+      attr_accessor :st
+
+      setup do
+        @st = SymbolTable.new
+      end
+
+      should "exist" do
+        st = SymbolTable.new
+        assert_not_nil st
+      end
+
+      should "have base scope" do
+        assert_equal st.current_scope, 0
+      end
+
+      should "store an empty variable" do
+        st.insert :a
+        assert_equal st.lookup(:a), nil
+      end
+
+      should "store an assigned variable" do
+        st.insert :b, 7
+        assert_equal 7, st.lookup(:b)
+      end
+
+    end
   end
 end
