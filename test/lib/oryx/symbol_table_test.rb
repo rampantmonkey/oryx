b5cfc7b2916f7da64dc1d43a1160fa187d2076de
Casey Robinson <kc@rampantmonkey.com>
2013-04-02 19:27:26 -0400

    start testing scope resolution

--- test/lib/oryx/symbol_table_test.rb
+++ test/lib/oryx/symbol_table_test.rb
@@ -27,7 +27,27 @@ module Oryx
         st.insert :b, 7
         assert_equal 7, st.lookup(:b)
       end
-
     end
+
+    context "scope resolution" do
+      attr_accessor :st
+
+      setup do
+        @st = SymbolTable.new
+        {a: 1, b:2, c:3, d:4, e:'a', f: 1.2}.each{ |k, v| st.insert k, v }
+      end
+
+      should "increase the scope" do
+        st.enter_scope
+        assert_equal 1, st.current_scope
+      end
+
+      should "raise an exception when exiting global scope" do
+        assert_raise RuntimeError do
+          st.exit_scope
+        end
+      end
+    end
+
   end
 end
