5d5e9eac3a98d44833c426b48e9a034093a78349
Casey Robinson <kc@rampantmonkey.com>
2013-04-04 15:51:38 -0400

    fix typo in error hierarchy

--- lib/oryx/error.rb
+++ lib/oryx/error.rb
@@ -4,5 +4,5 @@ module Oryx
   class LexError < Error; end
   class ParseError < Error; end
   class GenerationError < Error; end
-  class SybmolTableError < Error; end
+  class SymbolTableError < Error; end
 end
