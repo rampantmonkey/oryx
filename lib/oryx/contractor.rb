d44b0871ec39d517067718c2af574e790730ff6e
Casey Robinson <kc@rampantmonkey.com>
2013-04-12 09:46:55 -0400

    start of contractor class

---
+++ lib/oryx/contractor.rb
@@ -0,0 +1,45 @@
+require_relative "../oryx"
+
+require "rltk/cg/contractor"
+require "rltk/cg/llvm"
+require "rltk/cg/module"
+
+RLTK::CG::LLVM.init(:x86)
+
+module Oryx
+  class Contractor < RLTK::CG::Contractor
+    attr_reader :module, :st
+
+    def initialize
+      super
+      @module = RLTK::CG::Module.new('Oryx JIT')
+      @st = SymbolTable.new
+    end
+
+    def begin ast
+      puts ast.length
+      puts ast.class
+      ast.each {|a| dispatch a}
+    end
+
+    on Function do |node|
+      st.enter_scope
+      puts node.i
+      node.params.params.each {|p| visit p}
+      puts node.body, node.return_type
+    end
+
+    on Variable do |node|
+      puts "#{node.type} #{node.name}"
+    end
+
+
+    private
+      def dispatch node
+        case node
+        when Function then visit node
+        else raise GenerationError "Unhandled node type #{node}"
+        end
+      end
+  end
+end
